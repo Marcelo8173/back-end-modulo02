c62300d8397714ce32213e8492eea81520e8c5b6
Marcelo Andre <marceloandrebio@gmail.com>
2021-06-26 12:46:31 -0300

    add multer in server

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from 'express'
 import { createCategoriesController } from '../modules/cars/useCases/createCategories'
 import { listCategoriesController } from '../modules/cars/useCases/listCategories'
+import { importCategoryController } from '../modules/cars/useCases/importCategory'
+
+import multer from 'multer'
 
 const categoriesRoutes = Router()
+const upoload = multer({
+  dest: './tmp'
+})
 
 categoriesRoutes.post('/', (request, response) => {
   return createCategoriesController.createCategories(request, response)
@@ -10,6 +16,10 @@ categoriesRoutes.post('/', (request, response) => {
 
 categoriesRoutes.get('/', (resquest, response) => {
   return listCategoriesController.list(resquest, response)
+})
+
+categoriesRoutes.post('/import', upoload.single('file'), (request, response) => {
+  return importCategoryController.create(request, response)
 })
 
 export { categoriesRoutes }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express'
-import { categoriesRoutes } from './routes/categories.routes'
-import { specificationRoutes } from './routes/specification.routes'
-
+import { routes } from './routes'
 const app = express()
 
 app.use(express.json())
-app.use('/categories', categoriesRoutes)
-app.use('/specification', specificationRoutes)
+app.use(routes)
 
 app.listen(8080, () => {
   console.log('api 8080')
